ecb8736d2a7455d78a0f3d265dbba992917a2270
ryouya <ryouya@localhost>
2025-11-10 18:58:21 +0900

    課題1,2,3,5,11の再提出をさせていただきます。

---
+++ src/main/java/curriculum_A/Quustion1.java
@@ -0,0 +1,30 @@
+package curriculum_A;
+
+public class Quustion1 {
+
+	public static void main(String[] args) {
+		
+		 // 各変数を作る（型と初期値）
+        byte byteNum = 10;
+        short shortNum = 1000;
+        int intNum = 100000;
+        long longNum = 10000000000L;  // longは最後にL！
+        float floatNum = 9.5f;        // floatは最後にf！
+        double doubleNum = 10.5;
+        char letter = 'A';            // 1文字は ' '
+        String letters = "Hello";     // 文字列は " "
+        boolean bool = true;          // true または false
+
+        // 出力して確認
+        System.out.println(byteNum);
+        System.out.println(shortNum);
+        System.out.println(intNum);
+        System.out.println(longNum);
+        System.out.println(floatNum);
+        System.out.println(doubleNum);
+        System.out.println(letter);
+        System.out.println(letters);
+        System.out.println(bool);
+	}
+
+}
---
+++ src/main/java/curriculum_A/Quustion11.java
@@ -0,0 +1,17 @@
+package curriculum_A;
+
+public class Quustion11 {
+
+	public static void main(String[] args) {
+		  // int型の変数aとbを作って値を入れる
+        int a = 10;
+        int b = 20;
+
+        // aがbより小さいかどうかをresultに代入
+        boolean result = a < b;
+
+        // resultの値を出力
+        System.out.println(result);
+	}
+
+}
